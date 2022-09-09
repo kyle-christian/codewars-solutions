fc1720a035347066994320e7d96b3708ecddf8b6
Kyle Christian <kylebolanos@gmail.com>
2022-09-09 13:13:53 -0700

    ones-and-zeros.js

---
+++ 7kyu/ones-and-zeros.js
@@ -0,0 +1,44 @@
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+
+// However, the arrays can have varying lengths, not just limited to 4.
+
+/* 
+PARAMETERS:
+an array of ones and zeros, the array can be more than 4 integers
+
+RESULTS:
+turn the array into its binary equivalent
+
+EXAMPLE:
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+
+PSEUDOCODE:
+take the array and join together, make sure it's a number, then convert binary number into integer using parseInt
+
+*/
+
+const binaryArrayToNumber = arr => {
+    return parseInt(arr.join(''), 2);
+};
+
+console.log(binaryArrayToNumber([1, 1, 1, 1]));
