d019673679204faf96d70a83524c876c51d0ffbe
Kyle Christian <kylebolanos@gmail.com>
2023-01-01 15:55:21 -0800

    added total-amount-of-points.js

---
+++ 8kyu/total-amount-of-points.js
@@ -0,0 +1,65 @@
+// Our football team has finished the championship.
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+//     if x > y: 3 points (win)
+//     if x < y: 0 points (loss)
+//     if x = y: 1 point (tie)
+
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+//     our team always plays 10 matches in the championship
+//     0 <= x <= 4
+//     0 <= y <= 4
+
+/* 
+
+PARAMETERS:
+
+given the score of both teams, assign points to the x team based on these conditions:
+
+RESULTS:
+
+    if x > y: 3 points (win)
+    if x < y: 0 points (loss)
+    if x = y: 1 point (tie)
+
+EXAMPLES:
+
+    assert.strictEqual(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]) , 30);
+    assert.strictEqual(points(["1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"]) , 10);
+    assert.strictEqual(points(["0:1","0:2","0:3","0:4","1:2","1:3","1:4","2:3","2:4","3:4"]) , 0);
+    assert.strictEqual(points(["1:0","2:0","3:0","4:0","2:1","1:3","1:4","2:3","2:4","3:4"]) , 15);
+    assert.strictEqual(points(["1:0","2:0","3:0","4:4","2:2","3:3","1:4","2:3","2:4","3:4"]) , 12);
+
+PSEUDOCODE:
+
+conditional function that adds to a sum based on the array given
+
+*/
+
+function points(games) {
+    let sum = 0;
+
+    for (let i = 0; i < games.length; i++) {
+        // console.log(+games[i][0] > +games[i][2])
+
+        if (+games[i][0] > +games[i][2]) {
+            sum += 3;
+        } else if (+games[i][0] < +games[i][2]) {
+            sum += 0;
+        } else {
+            sum++;
+        }
+    }
+
+    return sum;
+}
+
+points(["1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"])
