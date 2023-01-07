7256860fa88f01f21e2f5fb6037893f6c4f7e726
Kyle Christian <kylebolanos@gmail.com>
2023-01-07 15:51:46 -0800

    added alan-partridge-II-apple-turnover.js

---
+++ 8kyu/alan-partridge-II-apple-turnover.js
@@ -0,0 +1,28 @@
+// Task
+
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+// Note: Input will either be a positive integer (or a string for untyped languages).
+
+/* 
+
+PARAMETERS:
+given x
+
+RESULTS:
+if x squared is more than a 1000, "return It's hotter than the sun!!" else return "Help yourself to a honeycomb Yorkie for the glovebox"
+
+EXAMPLES:
+
+PSEUDOCODE:
+simple coniditional checking x
+
+*/
+
+// let apple = x => x**2 > 1000 ? `It's hotter than the sun!!` : `Help yourself to a honeycomb Yorkie for the glovebox`;
+
+function apple(x) {
+    console.log(x**2 > 1000 ? `It's hotter than the sun!!` : `Help yourself to a honeycomb Yorkie for the glovebox`)
+}
+
+apple(100);
