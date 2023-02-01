bb8c4bbab7f2625e8b29af99e2121ebc4916d2c8
Kyle Christian <kylebolanos@gmail.com>
2023-02-01 12:41:17 -0800

    added switch-it-up.js

---
+++ 8kyu/switch-it-up.js
@@ -0,0 +1,50 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+/* 
+
+PARAMETERS:
+given a number between 0-9
+
+RESULTS:
+return it in words
+
+EXAMPLES:
+    assert.strictEqual(switchItUp(1),"One");
+    assert.strictEqual(switchItUp(3),"Three");
+    assert.strictEqual(switchItUp(5),"Five");
+
+PSEUDOCODE:
+the problem gives the solution itself, use a switch statement
+
+*/
+
+function switchItUp(n) {
+    switch(n) {
+        case 1:
+            return 'One'
+        case 2:
+            return 'Two'
+        case 3:
+            return 'Three'
+        case 4:
+            return 'Four'
+        case 5:
+            return 'Five'
+        case 6:
+            return 'Six'
+        case 7:
+            return 'Seven'
+        case 8:
+            return 'Eight'
+        case 9:
+            return 'Nine'
+        default:
+            return 'Zero'
+    }
+}
