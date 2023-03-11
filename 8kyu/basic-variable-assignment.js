c429b319961f49ee24d5f747daf8a34f3da296d7
Kyle Christian <kylebolanos@gmail.com>
2023-03-11 09:55:31 -0800

    added basic-variable-assignment.js

---
+++ 8kyu/basic-variable-assignment.js
@@ -0,0 +1,9 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+
+console.log(name)
+
+// the original code had double equal signs between the variable name and variable itself. By doing so, we are asking the computer to compare the two which does not work with assignment.
