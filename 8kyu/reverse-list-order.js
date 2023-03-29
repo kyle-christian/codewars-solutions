829d2ed2d66bbac2c2a3b197a7b5e798fc5af378
Kyle Christian <kylebolanos@gmail.com>
2023-03-29 10:56:12 -0700

    added reverse-list-order.js

---
+++ 8kyu/reverse-list-order.js
@@ -0,0 +1,37 @@
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+// Examples (Input -> Output)
+
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+/* 
+
+PARAMETERS:
+given an array
+
+RESULTS:
+reverse it 
+
+EXAMPLES:
+    Test.assertSimilar(reverseList([1,2,3,4]), [4,3,2,1]);
+    Test.assertSimilar(reverseList([3,1,5,4]), [4,5,1,3]);
+
+PSEUDOCODE:
+for loop and push/pop to a new array
+
+*/
+
+function reverseList(list) {
+    let arr =[]
+    for (let i = list.length - 1; i >= 0; i--) {
+        arr.push(list.pop())
+    }
+
+    return arr;
+}
+
+reverseList([1, 2, 3, 4, 5]);
+
+// a much simpler solution
+
+const reverseListAlt = list => list.reverse();
