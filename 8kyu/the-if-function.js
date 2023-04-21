a6e11c623984872a84526e2837473301f67fdc4c
Kyle Christian <kylebolanos@gmail.com>
2023-04-21 12:50:11 -0700

    added the if function js

---
+++ 8kyu/the-if-function.js
@@ -0,0 +1,34 @@
+// Create a function called _if which takes 3 arguments: a value bool and 2 functions (which do not take any parameters): func1 and func2
+
+// When bool is truthy, func1 should be called, otherwise call the func2.
+// Example:
+
+// _if(true, function(){console.log("True")}, function(){console.log("false")})
+// // Logs 'True' to the console.
+
+/* 
+
+PARAMETERS:
+given a boolean, a func1, and a func2
+
+RESULTS:
+if bool is truthy, func1 should be called, otherwise call the other func
+
+EXAMPLES:
+
+
+PSEUDOCODE:
+
+*/
+
+function _if(bool, func1, func2) {
+    if (bool == true) {
+        return func1();
+    } else {
+        return func2();
+    }
+}
+
+// simpler solution
+
+const _ifAlt = (bool, func1, func2) => bool ? func1() : func2();
