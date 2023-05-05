448388dda479635ea921c104b52084cd14ec3604
Kyle Christian <kylebolanos@gmail.com>
2023-05-05 08:39:28 -0700

    added fixme replace all dots js

---
+++ 8kyu/fixme-replace-all-dots.js
@@ -0,0 +1,24 @@
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+// Task
+
+// Fix the bug so we can all go home early.
+// Notes
+
+// String str will never be null.
+
+/* 
+
+PARAMETERS:
+given a function
+
+RESULTS:
+debug
+
+EXAMPLES:
+
+PSEUDOCODE:
+
+*/
+const replaceDots = str => str.replaceAll('.', '-');
