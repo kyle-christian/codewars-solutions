0b7980bc3b3d6d0f0a764a3969b428178ded6525
Kyle Christian <kylebolanos@gmail.com>
2023-06-05 12:57:10 -0700

    added grasshopper function syntax debugging js

---
+++ 8kyu/grasshopper-function-syntax-debugging.js
@@ -0,0 +1,20 @@
+// Grasshopper - Function syntax debugging
+
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+/* 
+the code in question
+
+function main [verb, noun]
+  return verb + noun
+}
+
+*/
+
+function main (verb, noun) {
+    return `${verb}${noun}`;
+}
+
+// refactor
+
+const main = (verb, noun) => `${verb}${noun}`;
